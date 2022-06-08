2bbb46cab9b9005826dad3d321d2cc0137c57145
jpark <tmdeksdl@gmail.com>
2022-06-08 14:24:16 +0900

    state 처음 사용

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import {useState} from 'react'
 
 //component 이름은 대문자로 시작해야함
 //최상위 태그가 하나로 묶여 있어야함
@@ -13,9 +14,10 @@ function Header(props){
 }
 function Nav(props){
   const liTags = props.data.map((e) => {
-    return <li><a href={'/read/' + e.id} onClick={(e) => {
-        e.preventDefault();
-        props.onSelect();
+    return <li key={e.id}><a href={'/read/' + e.id} onClick={(evt) => {
+        evt.preventDefault();
+        props.onSelect(e.id);
+        // debugger;
       }
     }>{e.title}</a></li>
   })
@@ -41,19 +43,26 @@ function deleteHandler(){
   alert("delete")
 }
 function App() {
+  let [mode, setMode] = useState('WELCOME');
   const topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
   ]
+  let content = null
+  if (mode === "WELCOME"){
+    content = <Article title="welcome" body="hello, web"></Article>
+  } else if (mode === "READ"){
+    content = <Article title="read" body="hello, read"></Article>
+  }
   return (
     <div>
       <Header onSelect={() => {
-        alert("header!!");
+        setMode("WELCOME");
       }}></Header>
-      <Nav data={topics} onSelect={() => {
-        alert("Nav!")
+      <Nav data={topics} onSelect={(id) => {
+        setMode("READ");
       }}></Nav>
-      <Article title="welcome" body="hello, web"></Article>
+      {content}
       <ButtonGroup variant="contained" aria-label="outlined primary button group">
         <Button variant="outlined" onClick={createHandler}>create</Button>
         <Button variant="outlined" onClick={updateHandler}>update</Button>
