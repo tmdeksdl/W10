57409ef163e6586cd05484ccddbe130ea17e9bd9
jpark <tmdeksdl@gmail.com>
2022-06-16 19:04:51 +0900

    read의 route화

--- src/App.js
+++ src/App.js
@@ -4,47 +4,16 @@ import {useState} from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom'
-function Header(props){
-  return <header className={props.className}><h1><Link to="/" onClick={(evt)=>{    
-    props.onSelect();
-  }}>WWW</Link></h1></header>
-}
-
+import {Link, Routes, Route, useParams} from 'react-router-dom'
+import { Header } from './Header';
+import { Article } from './Article';
+import { Nav } from './Nav';
+import { Create } from './Create';
 const HeaderStyled = styled(Header)`
     border-bottom:1px solid gray;
     color:red;
 `;
 
-function Article(props){
-  return <article>
-    <h2>{props.title}</h2>
-    {props.body}
-  </article>
-}
-function Nav(props){
-  const liTags = props.data.map((e)=>{
-    return <li key={e.id}><Link to={'/read/'+e.id} onClick={(evt)=>{
-      props.onSelect(e.id);
-    }}>{e.title}</Link></li>
-  })
-  return <nav><ol>{liTags}</ol></nav>
-}
-function Create(props){
-  return <article>
-    <h2>Create</h2>
-    <form onSubmit={(evt)=>{
-      evt.preventDefault();
-      const title = evt.target.title.value;
-      const body = evt.target.body.value;
-      props.onCreate(title, body);
-    }}>
-      <p><input type="text" name="title" placeholder="title"></input></p>
-      <p><textarea name="body" placeholder="body"></textarea></p>
-      <p><input type="submit" value="Create"></input></p>
-    </form>
-  </article>;
-}
 function App() {
   const [mode,setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
@@ -53,6 +22,7 @@ function App() {
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
   ]);
+  
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB!"></Article>
@@ -65,46 +35,80 @@ function App() {
       }
     })[0];
     content = <Article title={topic.title} body={topic.body}></Article>
-  } else if(mode === 'CREATE'){
-    content = <Create onCreate={(title, body)=>{
-      const newTopic = {id:nextId, title, body}
-      const newTopics = [...topics]; 
+  }
+
+  return (
+    <div>
+      <HeaderStyled onSelect={headerHandler()}></HeaderStyled>
+      <Nav data={topics}  onSelect={navHandler()}></Nav>
+      <Routes>
+        <Route path="/" element={<Article title="Welcome" body="Hello, WEBb!"></Article>}></Route>
+        <Route path="/create" element={<Create onCreate={onCreateHandler()}></Create>}></Route>
+        <Route path="/read/:topic_id" element={<Read topics={topics}></Read>}></Route>
+      </Routes>
+      <ButtonGroup>
+        <Button component={Link} to="/create" variant="outlined" onClick={createHandler()}>Create</Button>        
+        <Button component={Link} to="/update"variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined" onClick={deleteHandler()}>Delete</Button>
+    </div>
+  );
+
+  function Read(props) {
+    const params = useParams();
+    const id = Number(params.topic_id);
+    const topic = props.topics.filter(e=>{
+      if(e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    return <Article title={topic.title} body={topic.body}></Article>
+  }
+  function onCreateHandler() {
+    return (title, body) => {
+      const newTopic = { id: nextId, title, body };
+      const newTopics = [...topics];
       newTopics.push(newTopic);
       setTopics(newTopics);
       setId(nextId);
       setMode('READ');
-      setNextId(nextId+1);
-    }}></Create>;
+      setNextId(nextId + 1);
+    };
   }
-  return (
-    <div>
-      <HeaderStyled onSelect={()=>{
-        setMode('WELCOME');
-      }}></HeaderStyled>
-      <Nav data={topics}  onSelect={(id)=>{
-        setMode('READ');
-        setId(id);
-      }}></Nav>
-      {content}
-      <ButtonGroup>
-        <Button component={Link} to="/create" variant="outlined" onClick={()=>{
-          setMode('CREATE');
-        }}>Create</Button>        
-        <Button component={Link} to="/update"variant="outlined">Update</Button>
-      </ButtonGroup>
-      <Button variant="outlined" onClick={()=>{
-        const newTopics = topics.filter((e)=>{
-          if(e.id === id){
-            return false;
-          } else {
-            return true;
-          }
-        });
-        setMode('WELCOME');
-        setTopics(newTopics);
-      }}>Delete</Button>
-    </div>
-  );
+
+  function navHandler() {
+    return (id) => {
+      setMode('READ');
+      setId(id);
+    };
+  }
+
+  function deleteHandler() {
+    return () => {
+      const newTopics = topics.filter((e) => {
+        if (e.id === id) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      setMode('WELCOME');
+      setTopics(newTopics);}
+  }
+
+  function createHandler() {
+    return () => {
+      setMode('CREATE');
+    };
+  }
+
+  function headerHandler() {
+    return () => {
+      setMode('WELCOME');
+    };
+  }
 }
 
 export default App;
