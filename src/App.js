a2ca303d0c481cb3a582961651238b9dfe32c3b2
jpark <tmdeksdl@gmail.com>
2022-06-16 18:31:41 +0900

    React-router-dom을 도입

--- src/App.js
+++ src/App.js
@@ -1,92 +1,108 @@
-// import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import {useState} from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
+import {Link} from 'react-router-dom'
+function Header(props){
+  return <header className={props.className}><h1><Link to="/" onClick={(evt)=>{    
+    props.onSelect();
+  }}>WWW</Link></h1></header>
+}
 
 const HeaderStyled = styled(Header)`
-  border-bottom: 1px solid yellow;
-  
+    border-bottom:1px solid gray;
+    color:red;
 `;
-//component 이름은 대문자로 시작해야함
-//최상위 태그가 하나로 묶여 있어야함
-function Header(props){
-  
-  return <header className={props.className}><h1><a href="/" onClick={(e)=>{
-    e.preventDefault();
-    props.onSelect();
-  }}>WWW</a></h1></header>
-}
-function Nav(props){
-  const liTags = props.data.map((e) => {
-    return <li key={e.id}><a href={'/read/' + e.id} onClick={(evt) => {
-        evt.preventDefault();
-        props.onSelect(e.id);
-        // debugger;
-      }
-    }>{e.title}</a></li>
-  })
-  return <nav>
-  <ol>
-      {liTags}
-  </ol>
-</nav>
-}
+
 function Article(props){
   return <article>
-  <h2>{props.title}</h2>
-  {props.body}
-</article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
 }
-function createHandler(){
-  alert("create")
+function Nav(props){
+  const liTags = props.data.map((e)=>{
+    return <li key={e.id}><Link to={'/read/'+e.id} onClick={(evt)=>{
+      props.onSelect(e.id);
+    }}>{e.title}</Link></li>
+  })
+  return <nav><ol>{liTags}</ol></nav>
 }
-function updateHandler(){
-  alert("update")
-}
-function deleteHandler(){
-  alert("delete")
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(evt)=>{
+      evt.preventDefault();
+      const title = evt.target.title.value;
+      const body = evt.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"></input></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>;
 }
 function App() {
-  //아래는 '구조 분해 할당'이라는 기술이래
-  // const [a,b] = [A, B]
-  // a = A, b = B
-  const [mode, setMode] = useState('WELCOME');
+  const [mode,setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
-    {id:1, title:'html', body:'html is...'},
-    {id:2, title:'css', body:'css is...'},
-  ]
-  let content = null
-  if (mode === "WELCOME"){
-    content = <Article title="welcome" body="hello, web"></Article>
-  } else if (mode === "READ"){
-    const topic = topics.filter(e => {
-      if (e.id===id){
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+  ]);
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>
+  } else if(mode === 'READ'){
+    const topic = topics.filter(e=>{
+      if(e.id === id) {
         return true;
+      } else {
+        return false;
       }
-      else return false
-    })[0]
-    console.log("topic", topic)
+    })[0];
     content = <Article title={topic.title} body={topic.body}></Article>
+  } else if(mode === 'CREATE'){
+    content = <Create onCreate={(title, body)=>{
+      const newTopic = {id:nextId, title, body}
+      const newTopics = [...topics]; 
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setId(nextId);
+      setMode('READ');
+      setNextId(nextId+1);
+    }}></Create>;
   }
   return (
     <div>
-      <HeaderStyled onSelect={() => {
-        setMode("WELCOME");
+      <HeaderStyled onSelect={()=>{
+        setMode('WELCOME');
       }}></HeaderStyled>
-      <Nav data={topics} onSelect={(id) => {
-        setMode("READ");
+      <Nav data={topics}  onSelect={(id)=>{
+        setMode('READ');
         setId(id);
       }}></Nav>
       {content}
-      <ButtonGroup variant="contained" aria-label="outlined primary button group">
-        <Button variant="outlined" onClick={createHandler}>create</Button>
-        <Button variant="outlined" onClick={updateHandler}>update</Button>
+      <ButtonGroup>
+        <Button component={Link} to="/create" variant="outlined" onClick={()=>{
+          setMode('CREATE');
+        }}>Create</Button>        
+        <Button component={Link} to="/update"variant="outlined">Update</Button>
       </ButtonGroup>
-      <Button variant="outlined" onClick={deleteHandler}>delete</Button>
-      
+      <Button variant="outlined" onClick={()=>{
+        const newTopics = topics.filter((e)=>{
+          if(e.id === id){
+            return false;
+          } else {
+            return true;
+          }
+        });
+        setMode('WELCOME');
+        setTopics(newTopics);
+      }}>Delete</Button>
     </div>
   );
 }
