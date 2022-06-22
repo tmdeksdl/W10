38f0d831b93fad55aa7b9ab3234b969a838fd234
jpark <tmdeksdl@gmail.com>
2022-06-22 10:46:29 +0900

    useState를 useReducer로 바꾸기

--- 0622/src/App.js
+++ 0622/src/App.js
@@ -1,59 +1,95 @@
 import "./App.css";
-function Left1() {
+import {useState, useReducer} from 'react'
+function Left1(props) {
   return (
     <div>
       <h1>Left1</h1>
-      <Left2></Left2>
+      <Left2
+        onUp={() => {
+          props.onUp();
+        }}
+      ></Left2>
     </div>
   );
 }
-function Left2() {
+function Left2(props) {
   return (
     <div>
-      <h1>Left1</h1>
-      <Left3></Left3>
+      <h1>Left2</h1>
+      <Left3
+        onUp={() => {
+          props.onUp();
+        }}
+      ></Left3>
     </div>
   );
 }
-function Left3() {
+
+function Left3(props) {
   return (
     <div>
       <h1>Left3</h1>
-      <button>+</button>
+      <button
+        onClick={() => {
+          props.onUp();
+        }}
+      >
+        +
+      </button>
     </div>
   );
 }
-function Right1() {
+function Right1(props) {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2></Right2>
+      <Right2 count={props.count}></Right2>
     </div>
   );
 }
-function Right2() {
+function Right2(props) {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3></Right3>
+      <Right3 count={props.count}></Right3>
     </div>
   );
 }
-function Right3() {
+function Right3(props) {
   return (
     <div>
       <h1>Right3</h1>
-      <div>0</div>
+      {props.count}
     </div>
   );
 }
+
 export default function App() {
+  const countReducer = (state, action) => {
+    if (action.type === "UP") {
+      return { ...state, value: state.value + action.step };
+    }
+    return state;
+  };
+  const initialState = {
+    value: 0
+  };
+  const [count, dispatch] = useReducer(countReducer, initialState);
+  const up = (step) => {
+    return { type: "UP", step: step };
+  };
+
+
   return (
     <div id="app">
       <h1>Root</h1>
       <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-        <Left1></Left1>
-        <Right1></Right1>
+        <Left1
+          onUp={() => {
+            dispatch(up(1))
+          }}
+        ></Left1>
+        <Right1 count={count.value}></Right1>
       </div>
     </div>
   );
